c1f24bc394294b4efe3648d9d741df64d1afb56d
Johnson Ojo <johnson.ojo@bell.ca>
2023-07-15 12:06:15 -0400

    frontend architecture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>mycommerce</div>
-      <main></main>
-      <footer>All rights reserved.</footer>
-    </>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>MyCommerce</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/Cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/Signin" className="nav-link">
+              Sign
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+
+      <main>
+        <Container className="mt-3">
+          <Outlet />
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved.</div>
+      </footer>
+    </div>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.tsx";
+import "./index.css";
+import HomePage from "./pages/HomePage.tsx";
+import ProductPage from "./pages/HomePage.tsx";
+//const router = createBrowserRouter(...): Creates a router instance using the createBrowserRouter function from react-router-dom.
+//The router is responsible for handling the routing logic in the application.
+//createRoutesFromElements(...): Takes a JSX element and converts it into an array of route objects that can be consumed by the router.
+//In this case, it wraps the <App /> component inside a
+//createRoutesFromElements(...): Takes a JSX element and converts it into an array of route objects that can be consumed by the router. In this case,
+// it wraps the <App /> component inside a
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
